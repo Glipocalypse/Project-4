92c71d7bf47a2fc567310da7333e040ed07d79a8
Glipocalypse <mastahsteven@indy.rr.com>
2014-11-03 14:30:46 -0500

    Created base files

---
+++ FrankenyProject4.js
@@ -0,0 +1,22 @@
+// JavaScript File
+/*
+    Filename:  Loopiness
+    Written by: Steven Frankeny
+    Purpose: Learning and Having Fun
+    Date: 6 Oct. 2014
+    Modification History: None
+    Last Modified: N/A
+*/
+$(document).ready(function(){
+	$("#btnStartGame").click(startGame);
+	$("#btnInstructions").click(displayInstructions);
+	function startGame(){
+		alert("Lol, dumbass thought this would start a game.");
+	}
+	function displayInstructions(){
+		alert("Only noobs need instructions. Learn as you play!");
+	}
+	
+});
+
+	

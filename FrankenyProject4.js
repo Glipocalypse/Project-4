072cd159e33c532a6ca1eaeab8bdcdd2ef9553d8
Glipocalypse <mastahsteven@indy.rr.com>
2014-11-10 12:18:37 -0500

    Added game over condition

--- FrankenyProject4.js
+++ FrankenyProject4.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 	var gameSquareID = "SquareOne";
 	var numSquaresTapped = 0;
 	var timeBetweenSquares = 2000;
+	var gameNotOver = true;
 	$("#instructionsScreen").hide();
 	$("#gameScreen").hide();
 	$("#btnStartGame").click(startGame);
@@ -24,6 +25,7 @@ $(document).ready(function(){
 		if ($(this).is(':visible')){
 			$(this).hide();
 			numSquaresTapped += 1;
+			$(this).toggleClass("notTapped isTapped");
 			timer = new Date();
 			$("#squareCount").html("Squares Tapped: " + numSquaresTapped + "<br/> Taps per Second: " + (1000*numSquaresTapped/(timer.getTime() - startTime)).toFixed(3));
 			
@@ -49,49 +51,58 @@ $(document).ready(function(){
 		$("#menuScreen").show();
 		$("#instructionsScreen").hide();
 	}
-	function timeout_trigger() {
-		do{
-			gameSquareNum = Math.floor((Math.random() * 9) + 1);
-			switch(gameSquareNum){
-				case 1:
-					gameSquareID = "#SquareOne";
-					break;
-				case 2:
-					gameSquareID = "#SquareTwo";
-					break;
-				case 3:
-					gameSquareID = "#SquareThree";
-					break;
-				case 4:
-					gameSquareID = "#SquareFour";
-					break;
-				case 5:
-					gameSquareID = "#SquareFive";
-					break;
-				case 6:
-					gameSquareID = "#SquareSix";
-					break;
-				case 7:
-					gameSquareID = "#SquareSeven";
-					break;
-				case 8:
-					gameSquareID = "#SquareEight";
-					break;
-				case 9:
-					gameSquareID = "#SquareNine";
-					break;
-			}
-		}while($(gameSquareID).attr("class")=="gameSquare isNotAvailable");
-		$(gameSquareID).toggleClass("isNotAvailable isAvailable");
-		$(gameSquareID).show();
-		if (timeBetweenSquares > 100)
-			timeBetweenSquares = Math.floor(timeBetweenSquares*.99);
-		setTimeout(timeout_trigger, timeBetweenSquares);
-		$(gameSquareID).fadeOut(timeBetweenSquares*3, releaseSquare);
-	}
-	function releaseSquare(){
-		$(this).toggleClass("isNotAvailable isAvailable");
+	function timeout_trigger(){
+		if (gameNotOver)
+		{
+			do{
+				gameSquareNum = Math.floor((Math.random() * 9) + 1);
+				switch(gameSquareNum){
+					case 1:
+						gameSquareID = "#SquareOne";
+						break;
+					case 2:
+						gameSquareID = "#SquareTwo";
+						break;
+					case 3:
+						gameSquareID = "#SquareThree";
+						break;
+					case 4:
+						gameSquareID = "#SquareFour";
+						break;
+					case 5:
+						gameSquareID = "#SquareFive";
+						break;
+					case 6:
+						gameSquareID = "#SquareSix";
+						break;
+					case 7:
+						gameSquareID = "#SquareSeven";
+						break;
+					case 8:
+						gameSquareID = "#SquareEight";
+						break;
+					case 9:
+						gameSquareID = "#SquareNine";
+						break;
+				}
+			}while($(gameSquareID).attr("class").indexOf("isNotAvailable") >= 0);
+			$(gameSquareID).toggleClass("isNotAvailable isAvailable notTapped isTapped");
+			$(gameSquareID).show();
+			if (timeBetweenSquares > 100)
+				timeBetweenSquares = Math.floor(timeBetweenSquares*.99);
+			
+			setTimeout(timeout_trigger, timeBetweenSquares);
+			$(gameSquareID).fadeOut(timeBetweenSquares*3, checkTapped);
 		}
+	}
+	function checkTapped(){
+		if($(this).attr("class")=="gameSquare isNotAvailable notTapped"){
+			$(this).show();
+			$(this).css("background-color","#000000");
+			gameNotOver = false;
+		}
+		$(this).toggleClass("isNotAvailable isAvailable");
+	}
  
 	
 });
